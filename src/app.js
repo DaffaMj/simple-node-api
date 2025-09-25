3f098605fe8a4e1a108ed4657d3589a6f232e921
DaffaMj <daffastudent26@gmail.com>
2025-09-26 00:10:09 +0700

    add root route /

--- src/app.js
+++ src/app.js
@@ -15,9 +15,6 @@ app.use(express.json());
 
 // --- Data Sementara ---
 
-// 💾 Array untuk menyimpan item
-let items = ["Apple", "Banana", "Cherry", "Durian", "Elderberry"];
-
 // --- Routes/Endpoints ---
 
 // GET root (Tampilan Visual Selamat Datang dengan Tombol)
